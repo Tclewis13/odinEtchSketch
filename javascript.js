fe28d9c12011c7dfc92e3fa22310b828ffe0af6b
Tclewis13 <the3rdi@gmail.com>
2024-08-27 21:40:01 -0400

    main requirements done. moving on to extra credit.

--- javascript.js
+++ javascript.js
@@ -1,15 +1,36 @@
 const container = document.getElementById("container");
+const newGridBtn = document.getElementById("newGridBtn");
+newGridBtn.addEventListener('click', 
+    function() {
+        gridPrompt();
+    });
+newGrid(16);
 
-for (let y = 0; y<16; y++){
-    const rowContainer = document.createElement("div");
-    rowContainer.style.display = "flex";
-    rowContainer.style.justifyContent = "center";
-    rowContainer.style.alignItems = "center";
-    container.appendChild(rowContainer);
-    for(let x=0; x<16; x++){
-        let newButton = document.createElement("button");
-        newButton.style.height = '30px';
-        newButton.style.width = '30px';
-        rowContainer.appendChild(newButton);
+function newGrid(gridSize){
+    for (let y = 0; y<gridSize; y++){
+        const rowContainer = document.createElement("div");
+        rowContainer.style.display = "flex";
+        rowContainer.style.justifyContent = "center";
+        rowContainer.style.alignItems = "center";
+        rowContainer.style.flex = "1 1 0";
+        rowContainer.style.minWidth = "0";
+        container.appendChild(rowContainer);
+        for(let x=0; x<gridSize; x++){
+            let newButton = document.createElement("button");
+            newButton.style.alignSelf = "stretch";
+            newButton.style.flex = "1 1 0";
+            newButton.style.minWidth = "0";
+            newButton.addEventListener("mouseover", function (e) {
+                e.target.style.background = "blue";
+            });
+            rowContainer.appendChild(newButton);
+        }
     }
+}
+
+
+function gridPrompt(){
+    let gridSize = prompt("How many cells per side in new grid?");
+    document.getElementById("container").innerHTML = "";
+    newGrid(gridSize);
 }
